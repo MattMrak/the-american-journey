9a7356cb22f2f4b0b1f0aac33437e687edb657a0
MattMrak <matthewmrakovcic@gmail.com>
2021-06-24 12:19:23 -0400

    Login & Signup Button Redirect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useHistory } from "react-router-dom";
 
 function App() {
+
+  const history = useHistory();
+  
+  const handleLogin = () =>{ 
+    history.push("/login");
+  }
+
+  const handleSignup = () =>{ 
+    history.push("/signup");
+  }
+
   return (
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>
           The American Journey
         </h1>
-        <button>Login</button>
-        <button>Signup</button>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleSignup}>Signup</button>
       </header>
   );
 }
