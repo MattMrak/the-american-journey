8bd80728363eb4c8cfab2a1d7a2420eb5b897b85
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-06-29 14:10:07 -0400

    Fixed NewJourneyController

--- backend/app/controllers/new_journey_controller.rb
+++ backend/app/controllers/new_journey_controller.rb
@@ -1,42 +1,27 @@
 class NewJourneyController < ApplicationController
-    # before_action :redirect_if_not_logged_in
 
-    def new
-        @new_journey = NewJourney.new
-        @new_journey
+    def index
+        @new_journey = NewJourney.all
+        render json: @new_journey, status: 200
     end
 
     def create
-        @new_journey = current_user.new_journeys.build(new_journey_params)
-        if @new_journey.save
-            redirect_to new_journeys_path
-        else
-            render :new
-        end
-    end
-
-    def index
-        if params[:user_id] && @user = User.find_by_id(params[:user_id])
-            @new_journeys = @user.new_journeys
-        else
-            @new_journeys = NewJourney
-        end
+        @new_journey = NewJourney.create(new_journey_params)
+        render json: @new_journey, status: 200
     end
 
     def show
-        @new_journey = NewJourney.find_by_id(params[:id])
-        redirect_to new_journeys_path if !@new_journey
+        @new_journey = NewJourney.all
+        render json: @new_journey, status: 200
     end
 
     def destroy
-        @new_journey = NewJourney.find(params[:id])
         @new_journey.destroy
-        redirect_to new_journeys_path
     end
 
     private
     def new_journey_params
-        params.require(:new_journey).permit(:title, :description, :park_id, :user_id)
+        params.require(:new_journey).permit(:title, :description, :park_id, :user_id, :journey_id)
     end
 
 end
