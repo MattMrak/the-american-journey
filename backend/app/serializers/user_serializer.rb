654d9cee09a6d34afcce1352e359a73e06accf45
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-06-30 13:52:25 -0400

    Working on Singup.js , when form is submitted handleSubmit redirects to home page , need to get handleSubmit to send user info to backend API before redirect

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
     attributes :id, :email, :password
-    has_many :favorited_park
+    # has_many :favorited_park
 end
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
-
+import { addUser, addUserBE } from '../reducers/actions';
+// import { connect } from 'react-redux';
 class Signup extends Component {
+
+    state = {
+        user: ""
+    }
+
+    handleChange = (evt) => {
+        this.setState({
+          [evt.target.email]: evt.target.value
+        })
+      }
+    
+    handleSubmit = (evt) => {
+        evt.preventDefault()
+        // this.props.addUser(this.state)
+        // this.props.addUserBE(this.state.user)
+        this.props.history.push(`/home`)
+      }
+
     render() {
         return (
-            <form>
+            <div>
                 <h1>Signup</h1>
-                <div>
-                {/* <label>Email </label> */}
-                <input type="text" name="email" placeholder="Email" />
-                </div>
-                <div>
-                {/* <label>Password </label> */}
-                <input type="password" name="password" placeholder="Password" />
-                </div>
-                <input type="submit" value="Submit" />
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="user">Email</label>
+                    <input
+                        onChange={this.handleChange}
+                        type="text"
+                        email="user"
+                        // value={this.state.user}
+                        autoComplete="off"
+                        id="user"
+                    />
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={this.handleChange}
+                            type="text"
+                            password="password"
+                            // value={this.state.user}
+                            autoComplete="off"
+                            id="user"
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+
         );
     }
 }
 
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         users: () => { dispatch(addUserBE) }
+//     }
+// }
+
 export default Signup;
+
+// export default connect(null, { addUserBE, addUser, mapDispatchToProps })(Signup)
