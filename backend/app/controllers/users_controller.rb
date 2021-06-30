4b1f22216794adc1ee1b8e4c194091594ec1ce29
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-06-30 12:56:04 -0400

    Updated UsersController & created user actions.js in frontend

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,35 +1,24 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :destroy]
 
     def index
-      @users = User.all
+      @users = User.all 
       render json: @users
-    end
+  end
 
-    def show
+  def show
+      @user = User.find(params[:id])
       render json: @user
-    end
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :created, location: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
+  def create
+      @user = User.create(strong_params)
+      render json: @user
+  end
 
-    def destroy
-      @user.destroy
-    end
-    
   private
 
-  def set_user
-        @user = User.find(params[:id])
-      end
-
-      def user_params
-        params.require(:user).permit(:email, :password, :user_id)
-      end
+  def strong_params
+      params.permit(:email, :password, :user_id)
   end
+
+end
---
+++ frontend/src/reducers/actions.js
@@ -0,0 +1,32 @@
+export const addUserBE = (email) => {
+    console.log("from addUserBE", email)
+    return (dispatch) => {
+      fetch('http://localhost:3001/users',{
+             method:'POST',
+             headers: { 
+                 'Content-type': 'application/json',
+                 'accept': 'application/json'
+             },
+            body: JSON.stringify({
+                email: email
+              })
+      })
+        .then(resp => resp.json())
+        .then(user => {
+        dispatch(addUser(user))
+      })
+    }
+  }
+  
+  export const addUser = (newUser) => {
+    return {
+        type: "ADD_USER",
+        payload: newUser
+      }
+  }
+    
+  export const removeUser = () => {
+    return {
+      type: "CLEAR_USER"
+    }
+  }
