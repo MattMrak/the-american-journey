0d3f9448e7b009d5a5fe217fc546eba56ca946aa
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-02 16:57:35 -0400

    Added New Journey button to journeyinprogress page

--- frontend/src/components/JourneysInProgress.js
+++ frontend/src/components/JourneysInProgress.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
 import '../App.css';
+import { useHistory } from "react-router-dom";
 import Footer from './Footer';
-class JourneysInProgress extends Component {
-    render() {
-        return (
-            <div>
-                <header className="App-header">
-                    <h1>Journeys In Progress</h1>
-                </header>
-                <Footer/>
-            </div>
-        );
+
+const JourneysInProgress = () => {
+    const history = useHistory();
+    const handleNew = () => { 
+        history.push("/newjourney");
     }
+    return (
+        <div>
+            <header className="App-header">
+                <h1>Journeys In Progress</h1>
+                <button onClick={handleNew}>New Journey</button>
+            </header>
+            <Footer/>
+        </div>
+    );
 }
 
 export default JourneysInProgress;
-
-
