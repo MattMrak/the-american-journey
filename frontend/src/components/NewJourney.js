fbc23c09d946418a501590721d333fbc0a53fef5
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-06 14:27:58 -0400

    Got National Parks API w Key & Set up basic fetch to GET the map for NewJourney

--- frontend/src/components/NewJourney.js
+++ frontend/src/components/NewJourney.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Footer from './Footer';
+import { fetchMap, addMap } from '../reducers/actions';
+// import { connect } from 'react-redux';
 class NewJourney extends Component {
+    // state = {
+    //     user: ""
+    // }
     render() {
         return (
             <div>
                 <header className="App-header">
                     <h1>New Journey</h1>
                 </header>
+                {fetchMap}
+                {addMap}
                 <Footer/>
             </div>
         );
     }
 }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         users: () => { dispatch(fetchMap) }
+//     }
+// }
+
+// export default connect(null, { fetchMap, addMap, mapDispatchToProps })(NewJourney);
 
 export default NewJourney;
--- frontend/src/reducers/actions.js
+++ frontend/src/reducers/actions.js
@@ -1,32 +1,59 @@
 export const addUserBE = (email) => {
-    console.log("from addUserBE", email)
-    return (dispatch) => {
-      fetch('http://localhost:3001/users',{
-             method:'POST',
-             headers: { 
-                 'Content-type': 'application/json',
-                 'accept': 'application/json'
-             },
-            body: JSON.stringify({
-                email: email
-              })
-      })
-        .then(resp => resp.json())
-        .then(user => {
-        dispatch(addUser(user))
-      })
-    }
+  console.log("from addUserBE", email)
+  return (dispatch) => {
+    fetch('http://localhost:3001/users',{
+            method:'POST',
+            headers: { 
+                'Content-type': 'application/json',
+                'accept': 'application/json'
+            },
+          body: JSON.stringify({
+              email: email
+            })
+    })
+      .then(resp => resp.json())
+      .then(user => {
+      dispatch(addUser(user))
+    })
   }
+}
+
+export const addUser = (newUser) => {
+  return {
+      type: "ADD_USER",
+      payload: newUser
+    }
+}
   
-  export const addUser = (newUser) => {
-    return {
-        type: "ADD_USER",
-        payload: newUser
-      }
+export const removeUser = () => {
+  return {
+    type: "CLEAR_USER"
   }
-    
-  export const removeUser = () => {
-    return {
-      type: "CLEAR_USER"
+}
+
+export const fetchMap = (map) => {
+  console.log("from fetchMap", map)
+  return (dispatch) => {
+    fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=bMLSBoFZh0Rk0EwqA7kh58xcN4Qod0MbhFbvcgm8',{
+            method:'GET',
+            headers: { 
+                'Content-type': 'application/json',
+                'accept': 'application/json'
+            },
+          body: JSON.stringify({
+              map: map
+            })
+    })
+      .then(resp => resp.json())
+      .then(map => {
+      dispatch(addMap(map))
+    })
+  }
+}
+
+export const addMap = (newMap) => {
+  return {
+      type: "ADD_MAP",
+      payload: newMap
     }
-  }
+}
