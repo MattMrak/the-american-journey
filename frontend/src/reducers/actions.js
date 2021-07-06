041eee0ec6a91a4a43c110952f7f73b07870982d
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-06 14:41:42 -0400

    Added Fetch Actions for Favoriting Parks to Rails API Backend

--- frontend/src/reducers/actions.js
+++ frontend/src/reducers/actions.js
@@ -57,3 +57,50 @@ export const addMap = (newMap) => {
       payload: newMap
     }
 }
+
+export const fetchFavoritedToBE = (favorite) => {
+  console.log("from fetchMapToBE", favorite)
+  return (dispatch) => {
+    fetch('http://localhost:3001/favorited_parks',{
+            method:'POST',
+            headers: { 
+                'Content-type': 'application/json',
+                'accept': 'application/json'
+            },
+          body: JSON.stringify({
+              favorite: favorite
+            })
+    })
+      .then(resp => resp.json())
+      .then(favorite => {
+      dispatch(addFavorite(favorite))
+    })
+  }
+}
+
+export const addFavorite = (newFavorite) => {
+  return {
+      type: "ADD_Favorite",
+      payload: newFavorite
+    }
+}
+
+export const fetchFavoritedToFE = (favorite) => {
+  console.log("from fetchMapToBE", favorite)
+  return (dispatch) => {
+    fetch('http://localhost:3001/favorited_parks',{
+            method:'GET',
+            headers: { 
+                'Content-type': 'application/json',
+                'accept': 'application/json'
+            },
+          body: JSON.stringify({
+              favorite: favorite
+            })
+    })
+      .then(resp => resp.json())
+      .then(favorite => {
+      dispatch(favorite)
+    })
+  }
+}
