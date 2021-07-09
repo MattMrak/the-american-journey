60bab4db6244fffd88d06090255a0285dc5087fd
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-09 16:04:30 -0400

    Set up seed data for /all_parks

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#  t.string "full_name"
+# t.string "description"
+# t.string "contacts"
+# t.string "entrance_fees"
+# t.string "operating_hours"
+# t.string "addresses"
+# all parks ======= :id, :full_name, :description, :contacts, :entrance_fees, :operating_hours, :addresses
+
+AllPark.create(full_name: "Garfield Grove", description: "Hidden gem in the heart of Franklin Square", contacts: "516-790-9462", entrance_fees: "$5.00", operating_hours: "9am - 5pm", addresses: "902 Garfield Street")
--- frontend/src/actions/allParks.js
+++ frontend/src/actions/allParks.js
@@ -1,19 +1,33 @@
 export const fetchAllParks = (allParks) => {
-    console.log("from fetchAllParks", allParks)
-    return (dispatch) => {
-      fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=bMLSBoFZh0Rk0EwqA7kh58xcN4Qod0MbhFbvcgm8',{
-              method:'GET',
-              headers: { 
-                  'Content-type': 'application/json',
-                  'accept': 'application/json'
-              },
+  console.log("from fetchAllParks", allParks)
+  return (dispatch) => {
+    fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=bMLSBoFZh0Rk0EwqA7kh58xcN4Qod0MbhFbvcgm8')
+      .then(resp => resp.json())
+      .then(allParks => {
+        dispatch({
+          type: "GET_PARKS",
+          payload: allParks
+        })
       })
-        .then(resp => resp.json())
-        .then(allParks => {
-        dispatch(addAllParks(allParks))
-      })
-    }
   }
+}
+
+// export const fetchAllParks = (allParks) => {
+//     console.log("from fetchAllParks", allParks)
+//     return (dispatch) => {
+//       fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=bMLSBoFZh0Rk0EwqA7kh58xcN4Qod0MbhFbvcgm8',{
+//               method:'GET',
+//               headers: { 
+//                   'Content-type': 'application/json',
+//                   'accept': 'application/json'
+//               },
+//       })
+//         .then(resp => resp.json())
+//         .then(allParks => {
+//         dispatch(addAllParks(allParks))
+//       })
+//     }
+//   }
   
 export const addAllParks = (newParks) => {
     return {
