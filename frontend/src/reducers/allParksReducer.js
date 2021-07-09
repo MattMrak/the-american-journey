ac5a631564fae2896c68bad4337b906a86628288
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-09 16:10:10 -0400

    Edited allParksReducer

--- frontend/src/reducers/allParksReducer.js
+++ frontend/src/reducers/allParksReducer.js
@@ -1,11 +1,21 @@
-import React from 'react';
+const initialState = {
+    allParks: [],
 
-const allParksReducer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
 }
+
+
+const allParksReducer = (state = initialState, action) => {
+    switch(action.type){
+        case "ADD_PARK":
+            // return state.concat(action.payload)
+            return {
+                ...state,
+                allParks: [...state.allParks, action.payload]
+            }
+        default: 
+            return state
+    }
+}
+
 
 export default allParksReducer;
