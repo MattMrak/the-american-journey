98587dae95c44d096242a9b44c6f47f1813d5fb7
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-21 16:31:17 -0400

    created VisitedParkCard and set up so visited parks will be listed individually in a box

---
+++ frontend/src/components/VisitedParkCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class VisitedParkCard extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div 
+                    id={`visited-park-card-${this.props.id}`}
+                    style={{
+                            backgroundColor: `var(--${this.props.type})`, 
+                            }}
+                >
+                    <h1>{this.props.fullName}</h1>
+                    <p>Description: {this.props.description}</p>
+                    <p>Contacts: {this.props.contacts}</p>
+                    <p>Entrance Fees: {this.props.entranceFees}</p>
+                    <p>Operating Hours: {this.props.operatingHours}</p>
+                    <p>Addresses: {this.props.addresses}</p>
+                    <button>Delete</button>
+                    <br />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default VisitedParkCard;
--- frontend/src/components/VisitedParks.js
+++ frontend/src/components/VisitedParks.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Footer from './Footer';
+import {connect} from "react-redux"
+import { fetchVisitedParks } from '../actions/visitedParks';
+import VisitedParkCard from './VisitedParkCard';
 
 class VisitedParks extends Component {
+
+    state = {
+        visitedParks: []
+    }
+
     render() {
+        const visitedParksList = this.props.visitedParks.map(park => (
+            < VisitedParkCard
+                fullName={park.fullName}
+                description={park.description}
+                contacts={park.contacts}
+                entranceFees={park.entranceFees}
+                operatingHours={park.operatingHours}
+                addresses={park.addresses}
+            />
+        ))
         return (
             <div>
                 <header className="App-header">
-                    <h1>Visited Parks</h1>
+                    <div className="VisitedParksList">
+                        {visitedParksList}
+                    </div>
                 </header>
                 <Footer/>
             </div>
@@ -15,4 +35,10 @@ class VisitedParks extends Component {
     }
 }
 
-export default VisitedParks;
+const mapStateToProps = (stateFromStore) => {
+    return {
+        favoritedParks: stateFromStore.favoritedParksReducer.favoritedParks
+    }
+}
+
+export default connect(mapStateToProps, { fetchVisitedParks })(VisitedParks)
