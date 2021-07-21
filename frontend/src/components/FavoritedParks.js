a7e0efe77a013128392c88f3a4c5ef4bab591c3c
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-21 14:54:21 -0400

    created FavoritedParkCard and set up so favorited parks will be listed individually in a box

--- frontend/src/components/FavoritedParks.js
+++ frontend/src/components/FavoritedParks.js
@@ -3,6 +3,7 @@ import '../App.css';
 import Footer from './Footer';
 import {connect} from "react-redux"
 import { fetchFavoritedParks } from '../actions/favoritedParks';
+import FavoritedParkCard from './FavoritedParkCard';
 
 class FavoritedParks extends Component {
 
@@ -11,23 +12,22 @@ class FavoritedParks extends Component {
     }
 
     render() {
-        const favoritedParksNames = this.props.favoritedParks.map(park => <h1>{park.fullName}</h1>)
-        const favoritedParksDescriptions = this.props.favoritedParks.map(park => <p>{park.description}</p>)
-        const favoritedParksContacts = this.props.favoritedParks.map(park => <p>{park.contacts}</p>)
-        const favoritedParksEntranceFees = this.props.favoritedParks.map(park => <p>{park.entranceFees}</p>)
-        const favoritedParksOperatingHours = this.props.favoritedParks.map(park => <p>{park.operatingHours}</p>)
-        const favoritedParksAddresses = this.props.favoritedParks.map(park => <p>{park.addresses}</p>)
+        const favoritedParksNames = this.props.favoritedParks.map(park => (
+            < FavoritedParkCard
+                fullName={park.fullName}
+                description={park.description}
+                contacts={park.contacts}
+                entranceFees={park.entranceFees}
+                operatingHours={park.operatingHours}
+                addresses={park.addresses}
+                />
+        ))
         return (
             <div>
                 <header className="App-header">
                     <div className="FavoritedParksListDivTopLevel">
                         <div className="FavoritedParksListDiv">
                             {favoritedParksNames}
-                            Description: {favoritedParksDescriptions}
-                            Contacts: {favoritedParksContacts}
-                            Entrance Fees: {favoritedParksEntranceFees}
-                            Operating Hours: {favoritedParksOperatingHours}
-                            Addresses: {favoritedParksAddresses}
                         </div>
                     </div>
                 </header>
