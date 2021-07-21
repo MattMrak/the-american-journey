fb172dfbfaea6d9333c740be68ea62a641ec3aa2
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-21 16:35:32 -0400

    Set up action & reducer for visited parks fetch

--- frontend/src/actions/visitedParks.js
+++ frontend/src/actions/visitedParks.js
@@ -1,43 +1,13 @@
-export const fetchVisitedToBE = (visited) => {
-    console.log("from fetchVisitedToBE", visited)
-    return (dispatch) => {
-      fetch('http://localhost:3001/visited_parks',{
-              method:'POST',
-              headers: { 
-                  'Content-type': 'application/json',
-                  'accept': 'application/json'
-              },
-            body: JSON.stringify({
-                visited: visited
-              })
+export const fetchVisitedParks = () => {
+  return (dispatch) => {
+    fetch("http://localhost:3001/visited_parks")
+      .then(resp => resp.json())
+      .then(visitedParks => {
+        dispatch({
+          type: "GET_VISITED_PARKS",
+          payload: visitedParks
+        })
+        console.log("from fetchVisitedParks", visitedParks)
       })
-        .then(resp => resp.json())
-        .then(visited => {
-        dispatch(addVisited(visited))
-      })
-    }
-}
-  
-export const addVisited = (newVisited) => {
-    return {
-        type: "ADD_VISITED",
-        payload: newVisited
-      }
-}
-  
-export const fetchVisitedToFE = (visited) => {
-    console.log("from fetchVisitedToBE", visited)
-    return (dispatch) => {
-      fetch('http://localhost:3001/visited_parks',{
-              method:'GET',
-              headers: { 
-                  'Content-type': 'application/json',
-                  'accept': 'application/json'
-              }
-      })
-        .then(resp => resp.json())
-        .then(visited => {
-        dispatch(visited)
-      })
-    }
+  }
 }
--- frontend/src/reducers/visitedParksReducer.js
+++ frontend/src/reducers/visitedParksReducer.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
-const visitedParksReducer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
-
-export default visitedParksReducer;
+const initialState = {
+    visitedParks: [],
+  }
+  
+  const visitedParksReducer = (state = initialState, action) => {
+    switch(action.type){
+        case "GET_VISITED_PARKS":
+            return {
+                ...state,
+                visitedParks: [...state.visitedParks, ...action.payload]
+            }
+            default: 
+            return state
+    }
+  }
+  
+  export default visitedParksReducer;
