933c7074d8bd3695b8b8fd4f8dbe717b5eac61d4
Matthew Mrakovcic <matthewmrakovcic@gmail.com>
2021-07-27 13:45:54 -0400

    Working on backend destroy

--- backend/app/controllers/visited_parks_controller.rb
+++ backend/app/controllers/visited_parks_controller.rb
@@ -1,17 +1,18 @@
 class VisitedParksController < ApplicationController
+    before_action :set_visited_park, only: %i[show destroy]
+
     def index
         @visited_parks = VisitedPark.all
         render json: @visited_parks, status: 200
     end
 
+    def show
+        render json: @visited_park, status: 200
+    end
+    
     def create
         @visited_park = VisitedPark.create(visited_parks_params)
-        render json: @visited_parks, status: 200
-    end
-
-    def show
-        @visited_park = VisitedPark.all
-        render json: @visited_parks, status: 200
+        render json: @visited_park, status: 200
     end
     
     
@@ -19,11 +20,15 @@ class VisitedParksController < ApplicationController
     # """ @visited_park = VisitedPark.find(params[:id]) """ gives the error """ ActiveRecord::RecordNotFound (Couldn't find VisitedPark with 'id'=:id): """
     def destroy
         # binding.pry
-        @visited_park = VisitedPark.find(params[:id])
         @visited_park.destroy
     end
 
     private
+
+    def set_visited_park
+        @visited_park = VisitedPark.find(params[:id])
+    end
+
     def visited_parks_params
         params.permit(:id, :fullName, :description, :contacts, :entranceFees, :operatingHours, :addresses)
         # params.require(:visited_parks).permit(:id, :fullName, :description, :contacts, :entranceFees, :operatingHours, :addresses)
